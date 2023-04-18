bb8895c1655b698766939e6360f53057e4e16a58
Sarah Dombroski <danceprincess640@gmail.com>
2023-04-18 13:11:40 -0600

    Wrote Code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        int userGuess = 0;
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            userGuess = int.Parse(userInput);
+
+            if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        
+        Console.WriteLine("You guessed it!");
     }
 }
