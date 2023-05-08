20c47b5fd580eac6b4545f7b086831f93c00c49a
sarahdombroski640 <danceprincess640@gmail.com>
2023-05-08 12:10:11 -0600

    Wrote the Code

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new Assignment("Sarah", "Fractions");
+        string summary = a.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+
+        MathAssignment ma = new MathAssignment("Sarah", "Fractions", "7.3", "8-19");
+        string newsummary = ma.GetSummary();
+        string hmwklist = ma.GetHomeworkList();
+        Console.WriteLine(newsummary);
+        Console.WriteLine(hmwklist);
+        Console.WriteLine();
+
+        WritingAssignment wa = new WritingAssignment("Sarah", "European History", "The Causes of WWII");
+        string anothersummary = wa.GetSummary();
+        string writinglist = wa.GetWritingInformation();
+        Console.WriteLine(anothersummary);
+        Console.WriteLine(writinglist);
     }
 }
